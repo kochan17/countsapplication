e899e18124b0c39adab298d20da2d8f5c7e79a43
kota <i7811832616@gmail.com>
2021-03-24 20:59:45 +0900

    first commit

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countLabel.text = "0"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        count = count + 1
+        
+        countLabel.text = String(count)
+        
+        if count >= 10 {
+            chengeTextColor()
+        }
+    }
+    
+    
+    @IBAction func minus(_ sender: Any) {
+        count = count - 1
+        
+        countLabel.text = String(count)
+        
+        if count <= 0 {
+            resetColor()
+        }
+    }
+    
+    func chengeTextColor(){
+        
+        countLabel.textColor = .yellow
+    }
+    
+    func resetColor(){
+        
+        countLabel.textColor = .white
+    }
 }
 
